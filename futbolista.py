8073e9e805230ca4961679be502fa3845da0649b
Angelika Moya <angelmb023@gmail.com>
2023-10-14 23:27:30 -0500

    Update futbol

---
+++ futbolista.py
@@ -0,0 +1,35 @@
+from deportista import Deportista
+from persona import Persona
+
+class Futbolista(Persona, Deportista):
+    listaFutbolistas = []
+    
+    def __init__(self, nombre:str, edad:int, altura:str, sexo:str, anosPracticando:int,golesMarcados:int, tarjetasRojas:int, piernaHabil:str):
+        super().__init__(nombre,edad,altura,sexo)
+        super().__init__(anosPracticando)
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._piernaHabil = piernaHabil
+        
+        listaFutbolistas.append(self)
+        
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    
+    def setGolesMarcados(self, goles):
+        self._golesMarcados = goles
+    
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    
+    def setTarjetasRojas(self, tarjetas):
+        self._tarjetasRojas = tarjetas
+    
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    
+    def setPiernaHabil(self, pierna):
+        self._piernaHabil = pierna
+        
+    def __str__():
+        return f"Mi nombre es {super().getNombre()} soy profesional en el deporte {super().getDeporte()} Tengo {super().getEdad()} años de edad y llevo {super().getAñosPracticando()} años en el deporte"
